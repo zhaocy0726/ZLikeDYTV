150ebd0d421dd6b8fea906926e6c090d98f9c2c1
赵春阳 <zhaochunyang0726@163.com>
2016-09-14 12:10:07 +0800

    更新结构

---
+++ ZLikeDYTV/ZLikeDYTV/Live/empty.m
@@ -0,0 +1,13 @@
+//
+//  empty.m
+//  ZLikeDYTV
+//
+//  Created by 赵春阳 on 16/9/14.
+//  Copyright © 2016年 Z. All rights reserved.
+//
+
+#import "empty.h"
+
+@implementation empty
+
+@end
---
+++ ZLikeDYTV/ZLikeDYTV/Tools/empty.h
@@ -0,0 +1,13 @@
+//
+//  empty.h
+//  ZLikeDYTV
+//
+//  Created by 赵春阳 on 16/9/14.
+//  Copyright © 2016年 Z. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface empty : NSObject
+
+@end
